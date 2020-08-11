aa8431af4698cf28534c7592699d937bb718e7e3
Ariel <arielbs1991@gmail.com>
2020-08-10 19:46:26 -0700

    getting ready to call it. Gonna redeploy and write the readme

--- src/App.js
+++ src/App.js
@@ -75,32 +75,36 @@ class App extends Component {
         this.numInput = "";
         return (
             <div className="App">
-                <h1>Internal Spy Database</h1>
-                <label htmlFor="numInput">
-                    Number of Spies: 
-                <input
-                        id="numInput"
-                        name="numInput"
-                        value={this.state.numInput}
-                        type="number"
-                        min="0"
-                        onChange={this.handleInputChange}
-                    />
-                </label>
-                <button onClick={this.makeRequest} title={"Submit Number"} value="Submit Number">Submit Number</button>
-                <br />
-                <br />
-                <button onClick={this.sortEmployeesAlphebetical} title={"Sort Alphebetically"}>Sort Alphebetically</button>
+                <div class="jumbotron text-white bg-dark">
+                    <h1 class="display-4">Spies Emporium</h1>
+                    <p class="lead">You Need 'Em, We Got 'Em!</p>
+                    <div class="my-4">
+                        <label htmlFor="numInput">
+                            Number of Spies:
+                            <input
+                                id="numInput"
+                                name="numInput"
+                                value={this.state.numInput}
+                                type="number"
+                                min="0"
+                                onChange={this.handleInputChange}
+                            />
+                        </label>
+                        <button className={"button"} onClick={this.makeRequest} title={"Submit Number"} value="Submit Number">Render Spies</button>
+                    </div>
+                </div>
+                <button className={"button"} onClick={this.sortEmployeesAlphebetical} title={"Sort Alphebetically"}>Sort Alphebetically</button>
 
-                <button onClick={this.filterFemaleEmployees} title={"Sort by Women"}>Sort by Women</button>
+                <button className={"button"} onClick={this.filterFemaleEmployees} title={"Sort by Women"}>Sort by Women</button>
 
-                <button onClick={this.filterMaleEmployees} title={"Sort by Men"}>Sort by Men</button>
+                <button className={"button"} onClick={this.filterMaleEmployees} title={"Sort by Men"}>Sort by Men</button>
 
 
                 <div style={styles.employeeContainer}>
                     {this.renderEmployees()}
                 </div>
             </div>
+
         );
     }
 }
